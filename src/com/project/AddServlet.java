459db2440cc4ee815334f66931aa93965279fe79
Akshay-06 <akshayreddy1999@gmail.com>
2024-06-03 14:57:44 -0400

    Added code for calling Servlet from another Servlet

--- src/com/project/AddServlet.java
+++ src/com/project/AddServlet.java
@@ -3,20 +3,36 @@ package com.project;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	
+//	 We can control the post and get request.
+//	 If we want to handle only post request then method is doPost
+//	 If we want to handle only get request then method is doGet
+//	 service method works for both get and post methods
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		
 		int num1 = Integer.parseInt(req.getParameter("num1"));
 		int num2 = Integer.parseInt(req.getParameter("num2"));
 		
 		int result = num1+num2;
 		
-		PrintWriter out = res.getWriter();
-		out.println(result);
+		// Setting the value for attribute in req to send to next servlet
+		req.setAttribute("number", result);
+		
+		
+		// Calling the other servlet and passing req and res.
+		RequestDispatcher rd = req.getRequestDispatcher("square");
+		rd.forward(req, res);
+		
+//		PrintWriter out = res.getWriter();
+//		out.println(result);
 	}
 }
---
+++ src/com/project/SquareServlet.java
@@ -0,0 +1,22 @@
+package com.project;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SquareServlet extends HttpServlet{
+	
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int num = (int) req.getAttribute("number");
+		
+		int result = num * num;
+		
+		PrintWriter out = res.getWriter();
+		out.println("Result is "+result);
+	}
+}
